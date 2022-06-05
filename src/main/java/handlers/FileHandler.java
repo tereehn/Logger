579d1ee6811380450c0842a12e55caac4d0023a1
terez <tereza.ehnovaa@gmail.com>
2022-06-05 18:22:07 +0200

    base for rotation

--- src/main/java/handlers/FileHandler.java
+++ src/main/java/handlers/FileHandler.java
@@ -5,21 +5,39 @@ import java.io.*;
 import logger.LogRecord;
 
 public class FileHandler extends Handler {
-    private final String fileName;
-    private FileWriter fw = null;
-    private BufferedWriter bw = null;
-    private PrintWriter out = null;
+    protected final String fileName;
+    protected FileWriter fw = null;
+    protected BufferedWriter bw = null;
+    protected PrintWriter out = null;
+    protected String fileRoot =  "testdir/";
 
     public FileHandler(String fileName) { // create file
-        this.fileName = fileName;
+
+        File file = new File(fileRoot);
+
+        // true if the directory was created, false otherwise
+        if (file.mkdirs()) {
+            System.out.println("Directory is created!");
+        } else {
+            System.out.println("Failed to create directory!");
+        }
+        this.fileName = fileRoot+fileName;
         try {
-            fw = new FileWriter(fileName, true);
+            fw = new FileWriter(fileRoot+fileName, true);
             bw = new BufferedWriter(fw);
             out = new PrintWriter(bw);
-            //out.println("test");
+            out.println("test");
            // out.close();
         } catch (IOException e) {
         }
+    }
+
+    public String getFileRoot() {
+        return fileRoot;
+    }
+
+    public void setFileRoot(String fileRoot) {
+        this.fileRoot = fileRoot;
     }
 
     @Override
@@ -29,7 +47,6 @@ public class FileHandler extends Handler {
 
     @Override
     public void write(LogRecord record) {
-        System.out.println();
         out.println(record.toString());
         out.close();
     }
--- src/main/java/handlers/RotateFileHandler.java
+++ src/main/java/handlers/RotateFileHandler.java
@@ -1,12 +1,20 @@
 package handlers;
 
+import logger.LogRecord;
+
+import java.io.File;
+
 public class RotateFileHandler extends FileHandler {
 
     private int maxFileSize;
     private int maxFiles;
+    private int numOfFiles = 0;
+    private int currentFileSize = 0;
+
 
     public RotateFileHandler(String fileName) {
         super(fileName);
+
     }
 
     public RotateFileHandler(String fileName,int maxFileSize) {
@@ -34,5 +42,34 @@ public class RotateFileHandler extends FileHandler {
 
     public void setMaxFiles(int maxFiles) {
         this.maxFiles = maxFiles;
+    }
+
+    public int getNumOfFiles() {
+        return numOfFiles;
+    }
+
+    public void setNumOfFiles(int numOfFiles) {
+        this.numOfFiles = numOfFiles;
+    }
+
+    public int getCurrentFileSize() {
+        return currentFileSize;
+    }
+
+    public void setCurrentFileSize() {
+        File file = new File(fileName);
+        this.currentFileSize = (int) file.length();
+        System.out.println(this.currentFileSize);
+    }
+
+    @Override
+    public void write(LogRecord record) {
+        System.out.println("ROTATE");
+        if (this.getCurrentFileSize() >= this.getMaxFileSize()){
+            // vytvorime novy subor a stare v zlozke premenujeme
+        }
+        this.setCurrentFileSize();
+        super.write(record);
+        this.setCurrentFileSize();
     }
 }
