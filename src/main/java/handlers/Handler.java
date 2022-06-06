a3bf11712a29d0a98608c6ae7f542ad641dca1f5
terez <tereza.ehnovaa@gmail.com>
2022-06-06 10:17:25 +0200

    refactor file handling

--- src/main/java/handlers/Handler.java
+++ src/main/java/handlers/Handler.java
@@ -1,5 +1,6 @@
 package handlers;
 
+import java.io.IOException;
 import java.util.Formatter;
 import logger.LogRecord;
 
@@ -7,7 +8,7 @@ public abstract class Handler {
 
     protected Formatter formatter;
     public abstract void close(); // close the handlers.Handler and free all  resources
-    public abstract void write(LogRecord record); // Write a logger.LogRecord.
+    public abstract void write(LogRecord record) throws IOException; // Write a logger.LogRecord.
     public abstract void flush(); // Write a logger.LogRecord.
 
     public void addFormatter(Formatter formatter){
