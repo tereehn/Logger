a957b182436d34357e18829753d64b7ab213366f
terez <tereza.ehnovaa@gmail.com>
2022-06-17 11:57:39 +0200

    fix timestamp

--- src/main/java/util/TimeStamp.java
+++ src/main/java/util/TimeStamp.java
@@ -1,59 +1,27 @@
 package util;
 
 
-import util.Months;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TimeStamp  {
 
-    private final int year;
-    private final Months month;
-    private final int day;
-    private final int hour;
-    private final int minute;
-    private final int seconds;
+    private Date date;
+    private final SimpleDateFormat parser;
+    private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
-    public TimeStamp(int year, Months month, int day, int hour, int minute, int seconds) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        this.hour = hour;
-        this.minute = minute;
-        this.seconds = seconds;
-    }
-
-    public Months getMonth() {
-        return month;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public int getHour() {
-        return hour;
-    }
-
-    public int getMinute() {
-        return minute;
-    }
-
-    public int getSeconds() {
-        return seconds;
-    }
-
-    public int getYear() {
-        return year;
+    public TimeStamp(String input)  {
+        parser = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        try {
+            date = parser.parse(input);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String toString() {
-        return "TimeStamp{" +
-                "year=" + year +
-                ", month=" + month +
-                ", day=" + day +
-                ", hour=" + hour +
-                ", minute=" + minute +
-                ", seconds=" + seconds +
-                '}';
+        return "TimeStamp: " + formatter.format(date);
     }
 }
